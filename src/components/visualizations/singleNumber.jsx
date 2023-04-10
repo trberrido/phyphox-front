29cdba31912e9a3fdc7d510a05314b5875057d43
thibault <thibault.beerrido@gmail.com>
2023-04-10 14:15:18 +0000

    single number: scientific notation

--- src/components/visualizations/singleNumber.jsx
+++ src/components/visualizations/singleNumber.jsx
@@ -1,7 +1,10 @@
 
+import * as d3 from "d3";
+
 const VisualizationSingleNumber = (props) => {
+	const scientificNotation = d3.format('.2e')
 	return(<>
-		<p className='visualization-number__output'>{parseFloat(props.data).toFixed(2)}</p>
+		<p className='visualization-number__output'>{scientificNotation(parseFloat(props.data))}</p>
 		<p className='visualization-number__unit'>{props.unit}</p>
 	</>);
 }
