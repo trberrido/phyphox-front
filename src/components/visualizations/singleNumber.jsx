8e171d280199e3d9928163c77b984f9f04bce85f
Thibault Berrido <thibault.berrido@gmail.com>
2023-07-02 12:42:27 +0000

    single number: remove scientific notation

--- src/components/visualizations/singleNumber.jsx
+++ src/components/visualizations/singleNumber.jsx
@@ -1,10 +1,11 @@
 
-import * as d3 from "d3";
+//import * as d3 from "d3";
 
 const VisualizationSingleNumber = (props) => {
-	const scientificNotation = d3.format('.1e')
+	// const scientificNotation = d3.format('.1e');
+	// note : to display scientific notation: scientificNotation(parseFloat(props.data))
 	return(<>
-		<p className='visualization-number__output'>{scientificNotation(parseFloat(props.data))}</p>
+		<p className='visualization-number__output'>{parseFloat(props.data).toFixed(2)}</p>
 		<p className='visualization-number__unit'>{props.unit}</p>
 	</>);
 }
